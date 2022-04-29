fb7ce40734e017e236aa9710b3d29bca825e1b1d
AndreiMatveev <andreimatveev55@gmail.com>
2022-04-29 23:02:01 +0700

    Initial Commit

---
+++ iOS6-HW10-AndreiMatveev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS6-HW10-AndreiMatveev
+//
+//  Created by Владелец on 29.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
